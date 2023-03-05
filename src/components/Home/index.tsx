bb5bce86f7b37b9121c4af4cbc5139988a2ba692
ale-strada <79310707+ale-strada@users.noreply.github.com>
2023-03-05 18:37:51 -0300

    initial commit

---
+++ src/components/Home/index.tsx
@@ -0,0 +1,7 @@
+import { Header } from "@/components/header";
+
+export function HomePage(){
+    return <div>
+        <Header/>
+    </div>
+}
---
+++ src/components/header/index.tsx
@@ -0,0 +1,7 @@
+
+
+export function Header(){
+    return <div>
+        <img style={{width:"225px", height:"50px"}} src="	https://ingemix.com.ar/wp-content/uploads/2021/10/logohomh.jpg" alt="" />
+    </div>
+}
