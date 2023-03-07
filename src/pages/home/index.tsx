2df76dfe202a786d7d7125bb83577a04d2ac65d4
ale-strada <79310707+ale-strada@users.noreply.github.com>
2023-03-07 20:13:35 -0300

    fix  export home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,10 @@
-    import { Layout } from "@/components/layout";
+import { Layout } from "@/components/layout";
 import { Contacto } from "../../components/contacto";
 import { Referencias } from "../../components/referencias";
 import { Servicios } from "../../components/servicios";
 import { Trabajos } from "../../components/trabajos";
 
-export function HomePage(){
+export default function HomePage(){
     return <Layout>
             <div style={{display: "flex",  justifyContent: "center"}}>
                 <img style={{width:"100%", height:"80%", margin:"0"}} src="/banner 1.png" alt="" />
