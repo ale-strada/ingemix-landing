f0358065635f25054c9ad613862c07db94754311
ale-strada <79310707+ale-strada@users.noreply.github.com>
2023-03-09 12:44:48 -0300

    adedd carousel imgs

--- src/components/carousel-trabajos/index.tsx
+++ src/components/carousel-trabajos/index.tsx
@@ -5,7 +5,11 @@ import styled from 'styled-components';
 const FotoContainer = styled.div`
 height:180px;
 max-width:250px;
-border:solid 2px black;
+`
+const ImgCarousel = styled.img`
+height: 100%;
+width: 100%;
+object-fit: cover;
 `
 const CarouselItemContainer = styled.div`
 max-width:650px;
@@ -26,23 +30,70 @@ return (
     <Carousel activeIndex={index} onSelect={handleSelect}>
         <Carousel.Item>
             <CarouselItemContainer>
-                <FotoContainer/>
-                <FotoContainer/>
-                <FotoContainer/>
-                <FotoContainer/>
-                <FotoContainer/>
-                <FotoContainer/>
+                <FotoContainer>
+                    <ImgCarousel src="/img/carousel1.jpg" alt="" />
+                </FotoContainer>
+                <FotoContainer>
+                    <ImgCarousel src='/img/carousel2.jpg' alt=''/>
+                </FotoContainer>
+                <FotoContainer>
+                    <ImgCarousel src='/img/carousel3.jpg' alt=''/>
+                </FotoContainer>
+                <FotoContainer>
+                    <ImgCarousel src='/img/carousel4.jpg' alt=''/>
+                </FotoContainer>
+                <FotoContainer>
+                    <ImgCarousel src='/img/carousel5.jpg' alt=''/>
+                </FotoContainer>
+                <FotoContainer>
+                    <ImgCarousel src='/img/carousel6.jpg' alt=''/>
+                </FotoContainer>
             </CarouselItemContainer>
             <Carousel.Caption/>
         </Carousel.Item>
         <Carousel.Item>
             <CarouselItemContainer>
-                <FotoContainer/>
-                <FotoContainer/>
-                <FotoContainer/>
-                <FotoContainer/>
-                <FotoContainer/>
-                <FotoContainer/>
+                <FotoContainer>
+                    <ImgCarousel src="/img/carousel7.jpg" alt="" />
+                </FotoContainer>
+                <FotoContainer>
+                    <ImgCarousel src='/img/carousel8.jpg' alt=''/>
+                </FotoContainer>
+                <FotoContainer>
+                    <ImgCarousel src='/img/carousel9.jpg' alt=''/>
+                </FotoContainer>
+                <FotoContainer>
+                    <ImgCarousel src='/img/carousel10.jpg' alt=''/>
+                </FotoContainer>
+                <FotoContainer>
+                    <ImgCarousel src='/img/carousel11.jpg' alt=''/>
+                </FotoContainer>
+                <FotoContainer>
+                    <ImgCarousel src='/img/carousel12.jpg' alt=''/>
+                </FotoContainer>
+            </CarouselItemContainer>
+            <Carousel.Caption/>
+        </Carousel.Item>
+        <Carousel.Item>
+            <CarouselItemContainer>
+                <FotoContainer>
+                    <ImgCarousel src="/img/carousel13.jpg" alt="" />
+                </FotoContainer>
+                <FotoContainer>
+                    <ImgCarousel src='/img/carousel14.jpg' alt=''/>
+                </FotoContainer>
+                <FotoContainer>
+                    <ImgCarousel src='/img/carousel15.jpg' alt=''/>
+                </FotoContainer>
+                <FotoContainer>
+                    <ImgCarousel src='/img/carousel16.jpg' alt=''/>
+                </FotoContainer>
+                <FotoContainer>
+                    <ImgCarousel src='/img/carousel17.jpg' alt=''/>
+                </FotoContainer>
+                <FotoContainer>
+                    <ImgCarousel src='/img/carousel18.jpg' alt=''/>
+                </FotoContainer>
             </CarouselItemContainer>
             <Carousel.Caption/>
         </Carousel.Item>
