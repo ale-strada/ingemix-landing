c0fad537ab242f7e1c52afe5bf97930751914839
ale-strada <79310707+ale-strada@users.noreply.github.com>
2023-03-09 13:23:56 -0300

    subpage section ingenieria

--- src/components/ingenieria/index.tsx
+++ src/components/ingenieria/index.tsx
@@ -1,5 +1,18 @@
+import { SolucionContainer, SolucionesItem, SolucionesTitleText, SolucionLeft, SolucionRigth, SolucionTitleConteiner, SubpageImg } from "@/pages/soluciones/style";
+
+
 export function Ingenieria(){
-    return <div id="ingenieria">
-        ingenieria
-    </div>
+    return <SolucionContainer id="ingenieria">
+        <SolucionTitleConteiner color="#2E2E38">
+            <SolucionesTitleText>INGENIERÍA Y DESARROLLO</SolucionesTitleText>
+        </SolucionTitleConteiner>
+        <SolucionLeft  color="#F58634">
+            <SolucionesItem>Diseño conceptual de plantas industriales</SolucionesItem>
+            <SolucionesItem>Desarrollo de ingeniería básica</SolucionesItem>
+            <SolucionesItem>Gerenciamiento de proyectos</SolucionesItem>
+            <SolucionesItem>Obras civiles y estructuras</SolucionesItem>
+            <SolucionesItem>Elavoracion de ingeniería de detalle constructiva</SolucionesItem>
+        </SolucionLeft>
+        <SolucionRigth style={{border: "solid 2px"}}><SubpageImg src="/img/soluciones-ingenieria.png" alt="" /></SolucionRigth>
+    </SolucionContainer>
 }
---
+++ src/pages/soluciones/style.tsx
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+
+export const SolucionContainer = styled.div`
+width:90%;
+margin: 10px auto;
+display: grid;
+grid-template-columns: repeat(2, 1fr);
+grid-column-gap: 0px;
+grid-row-gap: 0px;
+gap:20px;
+` 
+export const SolucionTitleConteiner = styled.div`
+grid-area: 1 / 1 / 2 / 3;
+height: 100px;
+background-color:${props => props.color};
+display: flex;
+align-items: center;
+justify-content: center;
+`
+export const SolucionesTitleText = styled.h2`
+font-family: 'Roboto';
+color: #FFF;
+font-size: 30px;
+margin-bottom: 0px;
+`
+export const SolucionLeft = styled.div`
+grid-area: 2 / 1 / 3 / 2; 
+background-color:${props=>props.color};
+display: flex;
+flex-direction: column;
+justify-content: center;
+padding: 10px;
+`
+export const SolucionRigth = styled.div`
+grid-area: 2 / 2 / 3 / 3; 
+background-color:${props=>props.color};
+display: flex;
+flex-direction: column;
+justify-content: center;
+padding: 10px;
+`
+export const SubpageImg = styled.img`
+width:100%;
+height:100%;
+`
+export const SolucionesItem = styled.li`
+font-family: 'Roboto';
+font-size: 13px;
+margin: 10px;
+color:${props=>props.color}
+`
